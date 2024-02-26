0f5d467cf18977a497ab8a48fee1b3a8997a0db3
Wyyxx <a1121120071@utch.edu.mx>
2024-02-26 08:15:24 -0600

    Implementacion del programa V1

--- firebase.js
+++ firebase.js
@@ -1,17 +1,21 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+const firebaseApp = {
     apiKey: "AIzaSyAb79pHREcwgD3f7ONk4qvqJs4eR9iZ900",
     authDomain: "chat-app-7b002.firebaseapp.com",
     projectId: "chat-app-7b002",
     storageBucket: "chat-app-7b002.appspot.com",
     messagingSenderId: "1071937195050",
     appId: "1:1071937195050:web:82b2eaf3ce8047d3ee8753"
-};  
+}
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(auth);
+const app = initializeApp(firebaseApp);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+export { app, auth, db }
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Chat from '../components/Chat'
+import User from '../components/User'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h2>From app</h2>
+      <User/>
+      <Chat/>
     </div>
   )
 }
